7e5bd99945f7bb6cc133af72d20a57cc73a014d7
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-06-30 18:05:13 -0700

    installed node packages and created a database connection

---
+++ bamazonCustomer.js
@@ -0,0 +1,77 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+// -------- DATABASE CONNECTION CREDENTIALS------------
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "bamazon"
+});
+
+// -------- CONNECT TO DATABASE ------------
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+});
+
+// -------- QUERY PRODUCTS ------------
+function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
+
+/*
+// -------- USER INPUT ------------
+// Create a "Prompt" with a series of questions.
+inquirer
+  .prompt([
+    // Here we create a basic text prompt.
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "username"
+    },
+    // Here we create a basic password-protected text prompt.
+    {
+      type: "password",
+      message: "Set your password",
+      name: "password"
+    },
+    // Here we give the user a list to choose from.
+    {
+      type: "list",
+      message: "Which Pokemon do you choose?",
+      choices: ["Bulbasaur", "Squirtle", "Charmander"],
+      name: "pokemon"
+    },
+    // Here we ask the user to confirm.
+    {
+      type: "confirm",
+      message: "Are you sure:",
+      name: "confirm",
+      default: true
+    }
+  ])
+  .then(function(inquirerResponse) {
+    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+    if (inquirerResponse.confirm) {
+      console.log("\nWelcome " + inquirerResponse.username);
+      console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
+    }
+    else {
+      console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
+    }
+  });
+  */
